2a795ab62970a9ecc9e74197414007c1287a342a
Jan Dunder <jan.dunder@quanti.cz>
2023-09-20 15:13:22 +0200

    SEAT-12 - to twig added showing all seats of office and status if the seat is empty or not

--- assets/vue/controllers/OfficeDetails.vue
+++ assets/vue/controllers/OfficeDetails.vue
@@ -7,6 +7,8 @@
 		<ul>
 			<li v-for="item in office.seats" :key="item.id">{{ item }}</li>
 		</ul>
+		<b>entrypoint is: {{ entrypoint }}</b>
+		<p>Check out the <a :href="entrypoint" class="underline">API Docs</a></p>
 	</div>
 </template>
 
@@ -14,17 +16,37 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';  // Assuming you use Axios for HTTP requests
 
-const { officeId } = defineProps(['officeId']);
+const { officeId, entrypoint, seats } = defineProps(['officeId', 'entrypoint', 'seats']);
 const office = ref({});
-const seats = ref({});
 
 onMounted(async () => {
+	console.log(seats);
+
 	try {
-		const response = await axios.get(`https://localhost:8000/api/offices/${officeId}`);
+		const response = await axios.get(`${entrypoint}/offices/${officeId}`);
 		office.value = response.data;
-		console.log(response.data);
+		//console.log(response.data);
+
+		for (const item of office.value.seats) {
+			try {
+				const response = await axios.get(`${item}`);
+				//console.log(response.data);
+			}
+			catch (error) {
+				console.error("Failed to fetch office:", error);
+			}
+		}
 	} catch (error) {
 		console.error("Failed to fetch office:", error);
 	}
 });
+
+const getAssignments = async () => {
+	try {
+		const response = await axios.get('YOUR_ENDPOINT_URL_HERE');
+		data.value = response.data;
+	} catch (error) {
+		console.error("There was an error fetching the data:", error);
+	}
+};
 </script>
