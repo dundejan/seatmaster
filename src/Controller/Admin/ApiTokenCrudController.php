c810f593eb51803483a61a0819cc7e7a6da80ce6
Jan Dunder <jan.dunder@quanti.cz>
2023-10-13 13:00:13 +0200

    SEAT-15 - changed permissions to see and edit API tokens via admin

--- src/Controller/Admin/ApiTokenCrudController.php
+++ src/Controller/Admin/ApiTokenCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\ApiToken;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -13,6 +15,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class ApiTokenCrudController extends AbstractCrudController
 {
@@ -25,9 +28,6 @@ class ApiTokenCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-	        FormField::addPanel('Warning! Creating or editing API tokens manually is not recommended.')
-		        ->setHelp('If you plan to do that, make sure you know what you are doing ;-)')
-		        ->setFormTypeOption('disabled', true), // Disables the input field
             IdField::new('id')
 	            ->hideOnForm(),
 	        TextField::new('token')
@@ -35,12 +35,32 @@ class ApiTokenCrudController extends AbstractCrudController
 		        ->setDisabled(),
             AssociationField::new('ownedBy'),
             DateTimeField::new('expiresAt'),
+	        FormField::addPanel('Info:')
+		        ->setHelp('
+					<b>Token permissions: </b> API token has always the same permissions as it`s owner.<br>
+					<b>Visibility</b> API token is visible only to his owner. Only super-admins can see and edit all tokens.')
+		        ->setFormTypeOption('disabled', true),
         ];
     }
 
 	public function configureActions(Actions $actions): Actions
 	{
+		/**
+		 * @var User $thisUser
+		 */
+		$thisUser = $this->getUser();
+
+		// Condition function for showing API token user
+		$detailDisplayCondition = function (Action $action) use ($thisUser) {
+			$action->displayIf(static function (ApiToken $apiToken) use ($thisUser) {
+				return ($thisUser->hasRole('ROLE_SUPER_ADMIN') || $apiToken->getOwnedBy() === $thisUser);
+			});
+			return $action;
+		};
+
 		return parent::configureActions($actions)
+			->setPermission(Action::NEW, 'ROLE_SUPER_ADMIN')
+			->setPermission(Action::EDIT, 'ROLE_SUPER_ADMIN')
 			->disable(Action::BATCH_DELETE)
 			// disable delete on index page, it should be available just on edit page, where is shown warning
 			->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
@@ -49,6 +69,7 @@ class ApiTokenCrudController extends AbstractCrudController
 				});
 				return $action;
 			})
+			->update(Crud::PAGE_INDEX, Action::DETAIL, $detailDisplayCondition)
 			;
 	}
 
@@ -58,11 +79,59 @@ class ApiTokenCrudController extends AbstractCrudController
 			// the labels used to refer to this entity in titles, buttons, etc.
 			->setEntityLabelInSingular('API Token')
 			->setEntityLabelInPlural('API Tokens')
-			->setEntityPermission('ROLE_SUPER_ADMIN')
 
 			// the Symfony Security permission needed to manage the entity
 			// (none by default, so you can manage all instances of the entity)
 			// ->setEntityPermission('ROLE_EDITOR')
 			;
+	}
+
+	public function createEntity(string $entityFqcn)
+	{
+		/**
+		 * @var User $user
+		 */
+		$user = $this->getUser();
+
+		if (!$user->hasRole('ROLE_SUPER_ADMIN')) {
+			throw new AccessDeniedException('You can not edit yourself from admin section.');
+		}
+
+		return parent::createEntity($entityFqcn);
+	}
+
+	/**
+	 * @param ApiToken $entityInstance
+	 */
+	public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+	{
+		/**
+		 * @var User $user
+		 */
+		$user = $this->getUser();
+
+		if (!$user->hasRole('ROLE_SUPER_ADMIN')) {
+			throw new AccessDeniedException('You can not edit yourself from admin section.');
+		}
+
+		parent::updateEntity($entityManager, $entityInstance);
+	}
+
+	/**
+	 * @param ApiToken $entityInstance
+	 */
+	public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+	{
+		/**
+		 * @var User $user
+		 */
+		$user = $this->getUser();
+
+		if (!$user->hasRole('ROLE_SUPER_ADMIN')) {
+			throw new AccessDeniedException('You can not edit yourself from admin section.');
+		}
+
+
+		parent::deleteEntity($entityManager, $entityInstance);
 	}
 }
